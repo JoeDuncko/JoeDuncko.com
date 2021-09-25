9fc53ccc9d0d2b141f9db0997ca6b54936330fe3
Joe Duncko <JoeDuncko@users.noreply.github.com>
2021-09-24 21:30:04 -0400

    Install tailwind.css, remove existing styles

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# JoeDuncko.com
+
+My website, built via next.js + tailwind.css. Hosted on Vercel.
+
+## Developing
+
+- `npm install`
+- `npm run dev`
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
-import utilStyles from "../styles/utils.module.css";
 
 export default function Home({
   allPostsData,
@@ -20,23 +19,23 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section>
+        <h2>Blog</h2>
+        <ul>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small>
                 <Date dateString={date} />
               </small>
             </li>
