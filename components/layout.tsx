1f465998e81afa369a45189c7398b462eacb5027
Joe Duncko <JoeDuncko@users.noreply.github.com>
2022-02-23 11:14:51 -0500

    Hide footer when printing

--- components/layout.tsx
+++ components/layout.tsx
@@ -91,7 +91,7 @@ export default function Layout({
         </div>
       )}
 
-      <footer className="max-w-screen-lg sm:flex text-xs">
+      <footer className="max-w-screen-lg sm:flex text-xs print:hidden">
         <div className="text-center sm:text-left flex-1">
           {/* Fix height */}
           <FontAwesomeIcon icon={faCopyright} /> Joe Duncko{" "}
