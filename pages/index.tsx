deb5a708365286f7cbb0aec6538c8db62063d98b
Joe Duncko <JoeDuncko@users.noreply.github.com>
2022-02-26 21:13:37 -0800

    Fix gap

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,42 +28,44 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
 
-      {/* TODO: move this to its own component */}
-      <div className="mb-4 hidden justify-between print:flex">
-        <div>
-          <h1 className="text-4xl">Joe Duncko</h1>
-        </div>
-        <div>
-          <div className="text-right">
-            personal@JoeDuncko.com | (330) 719 - 1223 | JoeDuncko.com
+      {/* Wrapper that sets print width for whole resume */}
+      <div className="print:min-w-[960px]">
+        {/* TODO: move this to its own component */}
+        <div className="mb-4 hidden justify-between print:flex">
+          <div>
+            <h1 className="text-4xl">Joe Duncko</h1>
           </div>
-          <div className="text-right">
-            {socials.map((social, i, array) => (
-              <React.Fragment key={social.name}>
-                <FontAwesomeIcon icon={social.icon} />{" "}
-                {social.printFriendlyLabel}
-                {i !== array.length - 1 && " | "}
-              </React.Fragment>
-            ))}
+          <div>
+            <div className="text-right">
+              personal@JoeDuncko.com | (330) 719 - 1223 | JoeDuncko.com
+            </div>
+            <div className="text-right">
+              {socials.map((social, i, array) => (
+                <React.Fragment key={social.name}>
+                  <FontAwesomeIcon icon={social.icon} />{" "}
+                  {social.printFriendlyLabel}
+                  {i !== array.length - 1 && " | "}
+                </React.Fragment>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="sm:flex print:flex print:min-w-[960px]">
-        <div className="flex-1 sm:mr-4">
-          <Experiences />
+        <div className="flex flex-col sm:flex-row print:flex-row gap-8">
+          <div className="flex-1">
+            <Experiences />
+          </div>
+          <div className="flex-1">
+            <Education />
+            <Memberships />
+            <Projects />
+            <Interests />
+            <Socials className="print:hidden" />
+            <Contact className="print:hidden" />
+          </div>
         </div>
-        <div className="flex-1 sm:ml-4">
-          <Education />
-          <Memberships />
-          <Projects />
-          <Interests />
-          <Socials className="print:hidden" />
-          <Contact className="print:hidden" />
-        </div>
-      </div>
 
-      {/* <section>
+        {/* <section>
         <h2>Blog</h2>
         <ul>
           {allPostsData.map(({ id, date, title }) => (
@@ -79,6 +81,7 @@ export default function Home({
           ))}
         </ul>
       </section> */}
+      </div>
     </Layout>
   );
 }
