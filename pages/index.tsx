3d4246410d389a02bfa6ffa303b78b349ed64d5a
Joe Duncko <JoeDuncko@users.noreply.github.com>
2022-03-06 19:15:36 -0800

    Don't show gap between columns on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home({
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row print:flex-row gap-8">
+        <div className="flex flex-col md:flex-row print:flex-row md:gap-8">
           <div className="flex-1">
             <Experiences />
           </div>
