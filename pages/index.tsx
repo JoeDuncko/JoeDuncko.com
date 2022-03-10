34330e1ba2e659590bba277f60dec4f4ce770274
Joe Duncko <JoeDuncko@users.noreply.github.com>
2022-03-09 16:19:25 -0800

    Add socials to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
 import React from "react";
 import Layout from "../components/layout";
 import { siteTitle } from "../constants";
+import { socials } from "../data/socials";
 
 export default function Home() {
   return (
@@ -9,6 +11,24 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
+      <section>
+        <ul className="flex flex-row items-center text-center gap-8 justify-center">
+          {socials.map((social) => (
+            <li key={social.name}>
+              <a
+                href={social.link}
+                target="_blank"
+                title={`To Joe Duncko's ${social.name}`}
+              >
+                <FontAwesomeIcon icon={social.icon} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section>Some blog posts go here</section>
     </Layout>
   );
 }
