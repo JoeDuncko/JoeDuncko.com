784fca7071f7c1549797b1931e21ee3489408611
Joe Duncko <JoeDuncko@users.noreply.github.com>
2022-03-12 12:55:58 -0500

    Move nav

--- components/header.tsx
+++ components/header.tsx
@@ -5,23 +5,8 @@ import { description, name } from "../constants";
 export default function Header() {
   return (
     <header className="flex flex-col items-center mb-4 print:hidden">
-      <Image
-        priority
-        className="rounded-full"
-        src="/images/JoeDuncko2019Square.jpg"
-        height={144}
-        width={144}
-        alt={name}
-      />
-
-      <h1 className="text-4xl">{name}</h1>
-
-      <section className="flex flex-col items-center text-center">
-        <p>{description}</p>
-      </section>
-
-      <section>
-        <ul className="flex flex-row items-center text-center gap-8">
+      <nav className="mb-8">
+        <ul className="flex flex-row items-end text-center gap-8">
           {/* TODO: highlight the active page */}
           <li>
             <Link href="/">
@@ -39,6 +24,21 @@ export default function Header() {
             </Link>
           </li>
         </ul>
+      </nav>
+
+      <Image
+        priority
+        className="rounded-full"
+        src="/images/JoeDuncko2019Square.jpg"
+        height={144}
+        width={144}
+        alt={name}
+      />
+
+      <h1 className="text-4xl">{name}</h1>
+
+      <section className="flex flex-col items-center text-center">
+        <p>{description}</p>
       </section>
     </header>
   );
